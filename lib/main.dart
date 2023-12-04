6ee6ad3947b870e98a3a4b710c26e1108d473bf3
Turjo24 <turjodutta023@gmail.com>
2023-12-05 00:21:13 +0600

    =branch files were changed

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      title: 'pharmaconnect',
+      title: 'pharmaconnect Turjo',
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.frame2LoginScreen,
       routes: AppRoutes.routes,
